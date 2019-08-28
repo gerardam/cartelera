9749688a62f9efb401420130c363745a4b36f50d
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-08-27 22:45:31 -0500

    Visualizacion por genero

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from base.views import Home, HomeSinAcceso
+from base.views import Home, HomeSinAcceso, FilGenero
 
 urlpatterns = [
     # path('', Home.as_view(), name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='base/login.html'), name='logout'),
     path('sinacceso/', HomeSinAcceso.as_view(), name='sinacceso'),
+    path('genero/<int:gen_id>',FilGenero, name="gefiltro"),
 ]
--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin,\
         PermissionRequiredMixin
 from django.views import generic
-from cat.models import Pelicula
+from cat.models import Genero,Pelicula
 
 
 class SinAcceso(LoginRequiredMixin, PermissionRequiredMixin):
@@ -26,6 +26,11 @@ def Home(request):
     # template_name = 'base/home.html'
     # pelicula = Pelicula.objects.filter(edo=True)
     # context_object_name = 'obj'
+
+
+def FilGenero(request, gen_id=None):
+    pelicula = Pelicula.objects.filter(genero=gen_id)
+    return render(request, 'base/home.html', {'obj':pelicula})
 
 
 class HomeSinAcceso(LoginRequiredMixin, generic.TemplateView):
