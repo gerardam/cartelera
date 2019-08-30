84f34caf0c9c75a1723fa07f06ebb8e1973ab0f1
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-08-30 18:02:55 -0500

    cargar primer capitulo de la pagina uno

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from base.views import Home, HomeSinAcceso, FilGenero, FichaPeli
+from base.views import Home, HomeSinAcceso, FilGenero, FichaPeli,\
+    FilSerie, VerPeliSerie
 
 urlpatterns = [
     path('', Home, name='home'),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('sinacceso/', HomeSinAcceso.as_view(), name='sinacceso'),
     path('genero/<int:gen_id>', FilGenero, name="gefiltro"),
     path('ficha/<int:pel_id>', FichaPeli, name="peficha"),
+    path('serie/<int:ser_id>', FilSerie, name="sefiltro"),
+    path('serie/<int:ser_id>/cap/<int:pel_id>',VerPeliSerie, name="secapitulo"),
 ]
